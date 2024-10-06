4511479cd4167c98440faceb6f9778ee93795add
alex buz <alex_buz@yahoo.com>
2024-10-06 19:40:40 -0400

    generic

--- 17_pipeline.py
+++ 17_pipeline.py
@@ -13,9 +13,23 @@ init_config.init(**{})
 apc = init_config.apc
 
 apc.verbose = True
+import importlib.util
+import sys
+def import_pipeline(pipeline_name):
+ 
 
+    # Specify the path to the module (Python file)
+    module_name = "pipeline"
+    file_path = join("pipeline", pipeline_name+".py" )   
 
-def generate_topics(title, config):
+    # Load the module from the file path
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)   
+    return module 
+
+def execute_pipeline(title, pipeline_name,config):
 
 
     with open(config, 'r') as file:
@@ -25,9 +39,9 @@ def generate_topics(title, config):
     for key, val in vars.items( ):
         if val in globals():
             vars[key] = locals()[val]
-    pp(vars)
     
-    import pipeline.blog_writer as pipeline
+    pipeline= import_pipeline(pipeline_name)
+    response=[]
     for chat in pipeline.chats:
         pp(chat)
         agent=chat['agent']
@@ -39,63 +53,18 @@ def generate_topics(title, config):
         action_method=chat['action']
         action=getattr(agent, action_method) 
         agent_kwargs=chat.get('kwargs', {})
-        action(**agent_kwargs)
+        agent_response=action(**agent_kwargs)
+        response.append(agent_response)
         
 
-    exit()
-
-
-    #task = data['agents']['Writer']['task'].format(**vars)
-    #console.print(task, style="bold yellow")
-
-        
-    if 0:
-        writer = Writer( verbose=True)
-        
-        initial_response = writer.generate_reply('initial_task')
-    exit()    
-    if 1:
-        task_msg={"role": "user", "content": task}
-        writer_msg={"role": "assistant", "content": f'List of topics returned by writer:\n\n{writer.agent_response}\n\n'}
-        critic = Critic( verbose=True)
-        critic.add_history([task_msg, writer_msg])
-        critic_response = critic.reflect_with_llm()
-
-    exit()
-    if 1:
-        revision_task = data['agents']['Writer']['revision_task']
-        revised_response = writer.generate_reply(revision_task)
-
-    if 1:
-        seo_reviewer = Reviewer("SEO Reviewer", verbose=True)
-        seo_reviewer.add_history([task_msg, writer_msg])
-        seo_reviewer_response = seo_reviewer.reflect_with_llm()
-    if 1:
-        legal_reviewer = Reviewer("Legal Reviewer", verbose=True)
-        legal_reviewer.add_history([task_msg, writer_msg])
-        legal_reviewer_response = legal_reviewer.reflect_with_llm()
-    if 1:
-        ethics_reviewer = Reviewer("Ethics Reviewer", verbose=True)
-        ethics_reviewer.add_history([task_msg, writer_msg])
-        ethics_reviewer_response = ethics_reviewer.reflect_with_llm()
-    if 1:
-        seo_msg ={"role": "assistant", "content": f"SEO Reviewer Feedback:\n{seo_reviewer.agent_response}"}
-        legal_msg ={"role": "assistant", "content": f"Legal Reviewer Feedback:\n{legal_reviewer.agent_response}"}
-        ethics_msg ={"role": "assistant", "content": f"Ethics Reviewer Feedback:\n{ethics_reviewer.agent_response}"}
-            
-        meta_summarizer = Summarizer("Meta Summarizer", verbose=True)
-        meta_summarizer.add_history([task_msg, writer_msg, seo_msg, legal_msg, ethics_msg])
-        meta_summarizer_response = meta_summarizer.summarize()
-
-
-    if 1:
-        writer.add_history([{"role": "assistant", "content": f'Meta Summarizer Response:\n\n{meta_summarizer_response}'}])
-        final_task = revision_task = data['agents']['Writer']['final_task']
-        final_response = writer.generate_reply(final_task)
-    return final_response
+  
+    return response[1]
 
 
 if __name__ == '__main__':
-    config=join('config','topics.yaml')
+    py_pipeline_config='blog_writer'
+
+    yaml_pipeline_config=join('config','topics.yaml')
+
     title = "Building a Thriving Community: Collaborations and Initiatives at DeepLearning.AI"
-    generate_topics(title, config)
+    execute_pipeline(title, py_pipeline_config, yaml_pipeline_config)
